2086addfa527b1dd0e7bc4ee81d89d198c21f505
Muhammad Fath Mushaffa Azhar <87472508+fshaaa@users.noreply.github.com>
2021-12-15 01:43:17 +0700

    Membenarkan Back Button

--- src/id/ac/its/fpgame/breakout/AboutPanel.java
+++ src/id/ac/its/fpgame/breakout/AboutPanel.java
@@ -1,31 +1,29 @@
 package id.ac.its.fpgame.breakout;
 
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import id.ac.its.fpgame.breakout.MenuPanel.Click;
 
 public class AboutPanel extends JPanel implements Interface{
 	
-		JLabel bkg = new JLabel(); 
 	    JButton back = new JButton("Back");
-	    
+	    ImageIcon bk = new ImageIcon(this.getClass().getResource(PIC_BACK));
 	    Image menubkg = new ImageIcon (this.getClass().getResource(PIC_ABOUT)).getImage();
 	    
 	    AboutPanel(){
-	        setLayout(new FlowLayout()); 
+	        setLayout(null);
+	        back.setIcon(bk);
+	        back.setBounds(10, 507, 118, 50);
+	        back.setFocusPainted(false);
+	        back.setVisible(true);
 	        add(back);
-	        
 	        back.addMouseListener(new Click());
 	    }
 	   
