513ebee242e50586f40d6a11ed12f3622702ed9a
Muhammad Fath Mushaffa Azhar <87472508+fshaaa@users.noreply.github.com>
2021-12-15 00:42:25 +0700

    Membenarkan Tampilan UI

--- src/id/ac/its/fpgame/breakout/MenuPanel.java
+++ src/id/ac/its/fpgame/breakout/MenuPanel.java
@@ -1,57 +1,47 @@
 package id.ac.its.fpgame.breakout;
 
 import java.io.*;
-import java.net.URL;
 import javax.sound.sampled.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import javax.swing.event.*;
-import java.util.*;
-import java.awt.Color;
-import java.awt.Font;
  
 class MenuPanel extends JPanel implements Interface{
- 
-	JLabel bkg = new JLabel(); 
+
     JButton play = new JButton("");
     JButton about = new JButton("");
     JButton highscore = new JButton("");
-   
+    
     Image menubkg = new ImageIcon (this.getClass().getResource(PIC_MENU)).getImage();
-   
     ImageIcon playbtn = new ImageIcon (this.getClass().getResource (PIC_PLAYBUTTON));
     ImageIcon aboutbtn = new ImageIcon (this.getClass().getResource (PIC_ABOUTBUTTON));
     ImageIcon highscorebtn = new ImageIcon(this.getClass().getResource(PIC_HIGHSCOREBUTTON));
     
     MenuPanel(){
-    	setLayout(new FlowLayout());
-    	setFocusable(true);
+    	setLayout(null);
     	
         play.setIcon(playbtn);
         about.setIcon(aboutbtn);
         highscore.setIcon(highscorebtn);
         
+        play.setBounds(170, 280, 130, 50);
+        play.setVisible(true);
+        about.setBounds(170, 335, 130, 50);
+        about.setVisible(true);
+        highscore.setBounds(170, 390, 130, 50);
+        highscore.setVisible(true);
+        
         add(play);
         add(about);
         add(highscore);
-        
-        play.setVerticalTextPosition(SwingConstants.CENTER);
-        
+                
         play.addMouseListener(new Click());
         about.addMouseListener(new Click());
         highscore.addMouseListener(new Click());
     }
-   
-    class Click extends MouseAdapter{
+
+	class Click extends MouseAdapter{
         public void mouseClicked(MouseEvent me){
             if(me.getSource()== play){
                 Breakout.cl.show(Breakout.panel, "GamePanel");
