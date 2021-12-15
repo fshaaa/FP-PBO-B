e3a537866528addcad9bb2b78381b0de689a861e
Muhammad Fath Mushaffa Azhar <87472508+fshaaa@users.noreply.github.com>
2021-12-16 03:19:24 +0700

    Menambahkan HighScorePanel

--- src/id/ac/its/fpgame/breakout/Breakout.java
+++ src/id/ac/its/fpgame/breakout/Breakout.java
@@ -1,30 +1,37 @@
 package id.ac.its.fpgame.breakout;
 
 import java.awt.CardLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Breakout extends JFrame implements Interface{
-
+	
+	Image icon = new ImageIcon(this.getClass().getResource(PIC_BALL)).getImage();
 	static JPanel panel = new JPanel();
 	static CardLayout cl = new CardLayout();
 	static GamePanel game = new GamePanel();
 	static MenuPanel mp = new MenuPanel();
 	static AboutPanel ap = new AboutPanel();
+	static LevelPanel lp = new LevelPanel();
+	static HighscorePanel hp = new HighscorePanel();
 	
 	public Breakout() {
 		panel.setLayout(cl);
 		panel.add(mp, "MenuPanel");
 		panel.add(game, "GamePanel");
 		panel.add(ap, "AboutPanel");
+		panel.add(lp, "LevelPanel");
+		panel.add(hp, "HighscorePanel");
 		
 		cl.show(panel, "MenuPanel");
 		add(panel);
 		
+		setIconImage(icon);
 		setTitle("Breakout Game");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
-		
 		setSize(Interface.WIDTH, Interface.HEIGHT + 20);
 		setVisible(true);
 	}
